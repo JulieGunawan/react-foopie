19fc32102e303b5dc4477737509826bde0905ae4
JulieGunawan <julie.gunawan@protonmail.com>
2024-01-10 14:01:22 -0700

    modify the index.js to have a list of Foopies

--- src/index.js
+++ src/index.js
@@ -1,18 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Greeting() {
+function Foopies() {
   return (
-    <div>
-      <h1>Hello!</h1>
-      <ul>
-        <li>Apple</li>
-        <li>Banana</li>
-      </ul>
-    </div>
+    <>
+      <h1>Welcome to my Foopie Store!</h1>
+      <h2>
+        Foopie is a delicious combination of food and pie or dessert that you
+        never tried before
+      </h2>
+      <FoopieList />
+
+      <input type="text" />
+    </>
   );
 }
 
+const FoopieList = () => {
+  return (
+    <section>
+      <FoopieItem />
+      <FoopieItem />
+      <FoopieItem />
+    </section>
+  );
+};
+const FoopieItem = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Description />
+    </article>
+  );
+};
+
+const Image = () => {
+  return <img src="" alt="" />;
+};
+const Title = () => {
+  return <h3></h3>;
+};
+
+const Description = () => {
+  return <p></p>;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<Foopies />);
--- src/testing.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const testing = () => {
-  return <div>Testing</div>;
-};
-
-export default testing;
