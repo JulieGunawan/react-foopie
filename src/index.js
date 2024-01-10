ff7ad39016d9aca13fa87387ca64d10babc8102e
JulieGunawan <julie.gunawan@protonmail.com>
2024-01-10 16:53:40 -0700

    static array rendering

--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ const secondFoopie = {
   description: "A plate of cheese paired with chocolate cake and brownies",
 };
 
+const thirdFoopie = {
+  title: "Tayto Ice Cr3am",
+  img: "./assets/ice-cream-baked-potato.jpg",
+  description: "Hot steaming baked potato topped with ice creams and sprinkles",
+};
+
 function Foopies() {
   return (
     <>
@@ -35,9 +41,21 @@ function Foopies() {
 const FoopieList = () => {
   return (
     <section className="foopieList">
-      <FoopieItem title="Piezza" />
-      <FoopieItem title="Sweets" number={22} />
-      <FoopieItem title="Chocoloco" />
+      <FoopieItem
+        title={firstFoopie.title}
+        img={firstFoopie.img}
+        description={firstFoopie.description}
+      />
+      <FoopieItem
+        title={secondFoopie.title}
+        img={secondFoopie.img}
+        description={secondFoopie.description}
+      />
+      <FoopieItem
+        title={thirdFoopie.title}
+        img={thirdFoopie.img}
+        description={thirdFoopie.description}
+      />
     </section>
   );
 };
@@ -46,9 +64,9 @@ const FoopieItem = (props) => {
   console.log(props);
   return (
     <article className="foopieItem">
-      <img src={img} alt="chocolate pizza" />
+      <img src={props.img} alt="chocolate pizza" />
       <h3>{props.title}</h3>
-      <p>A slice of pizza topped with oreos, marshmallows, and icing sugar</p>
+      <p>{props.description}</p>
     </article>
   );
 };
