6e5703f6708eb05daa638a2216a7a9e8771b918e
JulieGunawan <julie.gunawan@protonmail.com>
2024-01-10 16:34:48 -0700

    use prop

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+const firstFoopie = {
+  title: "Pieeza",
+  img: "./assets/chocolate_pizza.jpg",
+  description:
+    "A slice of pizza topped with oreos, marshmallows, and icing sugar",
+};
+
+const secondFoopie = {
+  title: "Chocoloco",
+  img: "./assets/choco-cheese.jpg",
+  description: "A plate of cheese paired with chocolate cake and brownies",
+};
+
 function Foopies() {
   return (
     <>
@@ -29,7 +42,6 @@ const FoopieList = () => {
   );
 };
 
-const img = "./assets/chocolate_pizza.jpg";
 const FoopieItem = (props) => {
   console.log(props);
   return (
