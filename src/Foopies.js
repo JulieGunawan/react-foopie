79d0f030a4f59e75e2066911b3712f28f6882de4
JulieGunawan <julie.gunawan@protonmail.com>
2024-01-12 12:08:08 -0700

    move assets from public to src

--- src/Foopies.js
+++ src/Foopies.js
@@ -1,21 +1,25 @@
+import img1 from "./assets/chocolate_pizza.jpg";
+import img2 from "./assets/choco-cheese.jpg";
+import img3 from "./assets/ice-cream-baked-potato.jpg";
+
 const FoopieItems = [
   {
     id: 1,
     title: "Pieeza",
-    img: "./assets/chocolate_pizza.jpg",
+    img: img1,
     description:
       "A slice of pizza topped with oreos, marshmallows, and icing sugar",
   },
   {
     id: 2,
     title: "Chocoloco",
-    img: "./assets/choco-cheese.jpg",
+    img: img2,
     description: "A plate of cheese paired with chocolate cake and brownies",
   },
   {
     id: 3,
     title: "Tayto Ice Cr3am",
-    img: "./assets/ice-cream-baked-potato.jpg",
+    img: img3,
     description:
       "Hot steaming baked potato topped with ice creams and sprinkles",
   },
